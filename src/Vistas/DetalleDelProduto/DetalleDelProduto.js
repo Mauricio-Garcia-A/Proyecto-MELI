1f0fdecf5ffd3327663755ff94d2fbbce23f7924
Mauro32dx <mauromam32dx@gmail.com>
2022-04-05 19:13:35 -0300

    Se corrigieron erros de CSS y de funcionalidad

--- src/Vistas/DetalleDelProduto/DetalleDelProduto.js
+++ src/Vistas/DetalleDelProduto/DetalleDelProduto.js
@@ -16,9 +16,10 @@ import { productoSeleccionado, productosDescripcion, categoriasSelecionadas} fro
 */
 export default function DetalleDelProducto() {
     //Declaracion y inicalizacion de estados
-    const [ producto, setProducto ] = useState({})
-    const [ descripcionProducto, setDescripcionProducto ] = useState({})
-    const [ categorias, setCategorias ] = useState([])
+    const [ producto, setProducto ] = useState({});
+    const [ descripcionProducto, setDescripcionProducto ] = useState("");
+    const [ categorias, setCategorias ] = useState([]);
+    const [ fotoProducto, setFotoProducto] = useState("");
     const {id} = useParams();
 
 /* Llamadas al servicio API: BUSCARDETALLE - Hace las peticiones necesarias a la API para obtener 
@@ -29,31 +30,25 @@ export default function DetalleDelProducto() {
     async function buscarDetalle () {
         let product = await productoSeleccionado(id);               //Petición Y Respuesta de los detalles del producto productos 
         let productDescription = await productosDescripcion(id);    //Petición Y Respuesta de la descripcion del producto seleccionado 
-        setProducto(product)
-        setDescripcionProducto(productDescription) 
+        setProducto(product);
+        setFotoProducto(product.pictures[0].secure_url);
+        setDescripcionProducto(productDescription) ;
         let categories = await categoriasSelecionadas(product.category_id);    //Partición y Respuesta de las Categorías del producto seleccionado
         setCategorias(categories) 
     }
     buscarDetalle()
     }, [id])
 
-    console.log(producto)
-    console.log(descripcionProducto)
-    console.log(categorias)
-
-    const descripcionHardcodeados= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis non sem elementum tempor in at urna. Suspendisse auctor libero ut nibh consequat sed sagittis dolor iaculis. Donec condimentum mauris nec eros auctor sed vestibulum tellus consequat. Pellentesque tincidunt hendrerit neque, tincidunt tempus mauris consequat non.,"
     return (
         <>
             <BreadCrumbs Categorias={categorias} />
             <div  className='contenedorEstandarProducto'>
-                
-                                {/* Imagen={producto.pictures[0].secure_url}*/ }
-                <ProductoDetallado  Imagen={producto.thumbnail} 
+                <ProductoDetallado  Imagen={fotoProducto} 
                                     Estado={producto.value_name} 
                                     CantidadVendidos={producto.sold_quantity} 
                                     Titulo={producto.title} 
                                     Precio={producto.price} 
-                                    Descripcion={descripcionHardcodeados}
+                                    Descripcion={descripcionProducto}
                 />
             </div>
             <br />
--- src/Vistas/Error/Error.js
+++ src/Vistas/Error/Error.js
@@ -8,7 +8,7 @@ import {Link} from 'react-router-dom'
 export default function Error() {
     return (
         <div style={{ height:"1000px"}}>
-            <h1 style={{ color:"grey"}}> ERROR: La ruta no existe </h1>
+            <h1 style={{ color:"rgb(255, 42, 42)"}}> ERROR: La ruta no existe </h1>
             <Link to="/">Volver al HOME</Link>
         </div>
     )
