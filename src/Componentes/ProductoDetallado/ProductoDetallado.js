803ce99c5b02ee1f7003cbd8da4161b0ddd94685
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2022-07-04 14:58:19 -0300

    Se añade una ventana modal en el boton COMPRAR, en ProductoDetallado

--- src/Componentes/ProductoDetallado/ProductoDetallado.js
+++ src/Componentes/ProductoDetallado/ProductoDetallado.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CarrucelImagenProducto from '../CarrucelImagenProducto/CarrucelImagenProducto';
+import VentanaModal from '../VentanaModal/VentanaModal';
 import "./productoDetallado.scss";
+
 
 /* -----------------------------------   Componente PRODUCTODETALLADO   ---------------------------------------------
     Este componente recibe por props características mas especificas del producto [Titulo, Precio, Imagenes, Estado, CantidadVendida, Descripcion],
@@ -23,7 +25,9 @@ export default function ProductoDetallado(props) {
                     <span className='formatoTextoEstadoCantidad'>{props.Estado} - {props.CantidadVendidos} vendidos</span>
                     <b className='formatoTituloProducto'>{props.Titulo}</b>
                     <span className='formatoPrecio'>{tipoMoneda} {precioParteEntera}<sup className='superIndicePrecio'>{precioParteDecimal}</sup></span>
-                    <button className='botonComprar'>Comprar</button>                    
+                    <VentanaModal textoBoton='Comprar' estilosBoton='botonComprar' titulo='Proceso de Compra'>
+                        <ContenidoModal />
+                    </VentanaModal>                
                 </div>
             </div>
             <div className='contenedorDescripcionProducto'>
@@ -32,5 +36,25 @@ export default function ProductoDetallado(props) {
             </div>
             
         </>
+    )
+}
+
+
+const ContenidoModal= () => {
+    return(
+        <div className='contenidoModal'>
+            <span>¡Hola! Para comprar, ingresá a tu cuenta</span>
+            <div className='imgPerfil'>
+            <svg  width="90" height="90px" fill="rgba(255, 255, 255, 0.7)" style={{padding:'5px'}}  viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                </svg>
+            </div>
+            <span>Ingresá tu e-mail o usuario</span>
+            <input placeholder="Teléfono, e-mail o usuario"></input>
+            <button className='botonEstandar'>Continuar</button>
+            <button className='b2'>Crear cuenta</button>
+            <a href='/' className='b1'>Volver al HOME</a>  
+        </div>
     )
 }
